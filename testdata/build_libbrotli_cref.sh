52b127e1534f953098929d8036dc9a9ec8e3d2b7
Molecular Man <a.berezh@gmail.com>
2026-09-26 17:11:05 +0200

    ci: add 386 and arm64 to matrix

--- testdata/build_libbrotli_cref.sh
+++ testdata/build_libbrotli_cref.sh
@@ -21,7 +21,7 @@ for dir in common dec enc; do
     for src in "$ROOT"/brotli-ref/c/"$dir"/*.c; do
         base=$(basename "$src" .c)
         # C reference must round a*b before +c, same as the Go encoder.
-        cc -c -O2 -ffp-contract=off -I"$ROOT/brotli-ref/c/include" -o "$TMPDIR/${dir}_${base}.o" "$src"
+        cc ${CFLAGS:-} -c -O2 -ffp-contract=off -I"$ROOT/brotli-ref/c/include" -o "$TMPDIR/${dir}_${base}.o" "$src"
     done
 done
 
